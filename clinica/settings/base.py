3ea8ec97fa549436329559db2813a6ef8ffa09f0
Losmillo <n.berteuris@gmail.com>
2022-06-01 11:15:30 -0300

    arreglo debug 8

--- clinica/settings/base.py
+++ clinica/settings/base.py
@@ -123,6 +123,7 @@ LOGOUT_REDIRECT_URL = reverse_lazy ('login')
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Default primary key field type
